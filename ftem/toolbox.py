c9ec06751ab6c7a22a5ef83837ba23805b674965
japharel <japhet.arellanos43@gmail.com>
2023-02-22 21:09:55 -0600

    new v1.1.0
    updated plots for magnetic field, using sympy's code generation. as well, added new details for the graphs. Tema01 notebook now has the detailed graphs for each field

--- ftem/toolbox.py
+++ ftem/toolbox.py
@@ -7,7 +7,7 @@ class gravityField:
         self.mass = mass
         self.G = 6.67e-11
         
-    def vectorField(self, X, Y, Z = None, eqs = False):
+    def vectorField(self, X, Y, Z=None, eqs=False):
         # SI to cgs system conversion
         mGal = 1e5
         
@@ -47,17 +47,23 @@ class gravityField:
         else:
             pass
     
-    def plotVectorField2D(self, lim, samples, lines = True, field = False, cmap='viridis', color='black', aspect='equal'):
+    def plotVectorField2D(self, lim, samples, lines=True, field=False, cmap='viridis', color='black', aspect='equal'):
         X, Y = np.meshgrid(np.linspace(-lim, lim, samples),
                            np.linspace(-lim, lim, samples))
         G_R, G_X, G_Y = self.vectorField(X, Y)
         
-        if lines and not field:
-            plt.quiver(X, Y, G_X, G_Y)
+        if samples >= 80:
+            s = np.floor(np.power(samples, 1/3))
+        else:
+            s = 1
+        skip = (slice(None, None, s), slice(None, None, s))
+        
+        if lines and not field:    
+            plt.quiver(X[skip], Y[skip], G_X[skip], G_Y[skip])
             plt.gca().set_aspect(aspect)
         elif lines and field:
             plt.contourf(X, Y, G_R, samples, cmap=cmap)
-            plt.quiver(X, Y, G_X, G_Y, color=color)
+            plt.quiver(X[skip], Y[skip], G_X[skip], G_Y[skip], color=color)
             plt.gca().set_aspect(aspect)
         elif not lines and field:
             plt.contourf(X, Y, G_R, samples, cmap=cmap)
@@ -70,33 +76,67 @@ class magneticField:
         self.I = I
         self.mu0 = 4 * np.pi * 1e-7
         
-    def vectorField(self, x, y, z = None):
-        if not z:
-            r = np.hypot(x, y)# + x.max() / 3 * np.ones_like(x)
-            k = self.mu0 * self.I / (2 * np.pi)
+    def vectorField(self, X, Y, Z=None, eqs=False):
+        if not Z:
+            # sympy symbols
+            I, mu0, s, theta = sy.symbols('I mu_0 s theta')
             
-            b_x = - k * y / r
-            b_y = k * x / r
+            # analytic equations
+            # adapted from Tejero's notes
+            a_Bp = mu0 * I / (2 * sy.pi * s)
+            a_Bx = - mu0 * I / (2 * sy.pi * s) * sy.sin(theta)
+            a_By = mu0 * I / (2 * sy.pi * s) * sy.cos(theta)
             
-            return (b_x, b_y)
+            # analytic to numerical representation
+            n_Bp = sy.lambdify([I, s], a_Bp.subs({mu0:self.mu0}))
+            n_Bx = sy.lambdify([s, theta, I], a_Bx.subs({mu0:self.mu0}))
+            n_By = sy.lambdify([s, theta, I], a_By.subs({mu0:self.mu0}))
+            
+            # distance-r vector
+            # added noise is experimental after trial and error
+            # where the ratio of such noise was calibrated such
+            # that the field neither explodes at r = 0 nor
+            # is not visible as a whole within the range
+            S = np.hypot(X, Y) + X.max()/2 * np.ones_like(X)
+            T = np.arctan2(Y, X)
+            
+            # numerical vector components of field
+            Bp = n_Bp(self.I, S)
+            Bx = n_Bx(S, T, self.I)
+            By = n_By(S, T, self.I)
+            
+            if eqs:
+                display(a_Bx + a_By)
+                
+            return Bp, Bx, By
         else:
             pass
                 
     # Yet to implement: 3D Vector Field and plot
     
-    def plotVectorField2D(self, lim, samples, field = False):
+    def plotVectorField2D(self, lim, samples, lines = True, field = False, cmap='viridis', color='black', aspect='equal'):
         X, Y = np.meshgrid(np.linspace(-lim, lim, samples),
                            np.linspace(-lim, lim, samples))
-        B_X, B_Y = self.vectorField(X, Y)
+        B_P, B_X, B_Y = self.vectorField(X, Y)
         
-        if field:
-            plt.quiver(X, Y, B_X, B_Y)
-            plt.imshow(B_X + B_Y, origin = 'lower', 
-                       extent = (X.min(), X.max(), Y.min(), Y.max()))
+        if samples >= 80:
+            s = np.floor(np.power(samples, 1/3))
         else:
-            plt.quiver(X, Y, B_X, B_Y)
+            s = 1
+        skip = (slice(None, None, s), slice(None, None, s))
+        
+        if lines and not field:    
+            plt.quiver(X[skip], Y[skip], B_X[skip], B_Y[skip])
+            plt.gca().set_aspect(aspect)
+        elif lines and field:
+            plt.contourf(X, Y, B_P, samples, cmap=cmap)
+            plt.quiver(X[skip], Y[skip], B_X[skip], B_Y[skip], color=color)
+            plt.gca().set_aspect(aspect)
+        elif not lines and field:
+            plt.contourf(X, Y, B_P, samples, cmap=cmap)
+            plt.gca().set_aspect(aspect)
         plt.title('Campo magnético 2D')
-        plt.show() 
+        plt.show()
         
 if __name__ == '__main__':
     
@@ -114,4 +154,6 @@ if __name__ == '__main__':
     
     samples = 30
     lim = 10
-    b.plotVectorField2D(lim, samples, field=True)
+    b.plotVectorField2D(lim, samples)
+    b.plotVectorField2D(lim, samples, lines=False, field=True)
+    b.plotVectorField2D(lim, samples, True, True)
